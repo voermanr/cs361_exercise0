8a0b67fe22e4bdbedcb52c92f8b8ffaf36ed1835
kuijiboC <kuijibo@gmail.com>
2022-09-23 15:22:44 -0700

    updated regex

--- main.rb
+++ main.rb
@@ -5,7 +5,8 @@ def add(num1, num2)
 end
 
 def check_input(str)
-  return true if str =~ /\w{3} \d* \d*$/ || str =~ /quit$/
+  return true if
+    str =~ /(((add)|(sub)|(div)|(mul)) \d* \d*)|quit/
 
   puts "usage: add|sub|mul|div v0 v1\nquit"
   false
@@ -14,11 +15,23 @@ end
 str = ''
 
 while str != 'quit'
-  puts 'type your stuff'
+  print 'SuperMathyBot> '
   str = gets
   next unless check_input(str)
 
   str = str.chop
   cmds_arr = str.split
   puts cmds_arr
+  case cmds_arr[0]
+  when 'add'
+    add(cmds_arr[1], cmds_arr[2])
+  when 'sub'
+    sub(cmds_arr[1], cmds_arr[2])
+  when 'div'
+    div(cmds_arr[1], cmds_arr[2])
+  when 'mul'
+    mul(cmds_arr[1], cmds_arr[2])
+  else
+    'what have you done'
+  end
 end
