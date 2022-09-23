3ded467978e8fdc640d5bf64110e314db75deda5
kuijiboC <kuijibo@gmail.com>
2022-09-23 15:08:09 -0700

    implemented check_input() and tokenized input string

--- main.rb
+++ main.rb
@@ -4,13 +4,21 @@ def add(num1, num2)
   num1 + num2
 end
 
+def check_input(str)
+  return true if str =~ /\w{3} \d* \d*$/ || str =~ /quit$/
+
+  puts "usage: add|sub|mul|div v0 v1\nquit"
+  false
+end
+
 str = ''
 
 while str != 'quit'
   puts 'type your stuff'
   str = gets
+  next unless check_input(str)
+
   str = str.chop
-  cmd = str[0..2]
-  puts cmd
-  puts "str: #{str}"
+  cmds_arr = str.split
+  puts cmds_arr
 end
